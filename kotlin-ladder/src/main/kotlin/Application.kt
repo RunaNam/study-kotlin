d28db8dc0d1a5ac0113442448456731911e2be6e
minjeong <minjeong@woowahan.com>
2023-03-09 14:21:21 +0900

    feat: 사다리 생성 뷰 구현

--- kotlin-ladder/src/main/kotlin/Application.kt
+++ kotlin-ladder/src/main/kotlin/Application.kt
@@ -1,10 +1,31 @@
+import domain.ladder.Ladder
 import domain.player.Players
 import view.InputView
 import view.OutputView
 
 fun main() {
     val players = generatePlayers()
+    val ladder = generateLadder(players)
+
+    for (player in players.value) {
+        println(player.position)
+    }
+    
+    println()
+
+    for (a in ladder.value) {
+        println(a)
+    }
 }
+
+fun generateLadder(players: Players): Ladder =
+    try {
+        val height = InputView.inputHeight()
+        Ladder(height, players.value.size)
+    } catch (e: RuntimeException) {
+        OutputView.printException(e)
+        generateLadder(players)
+    }
 
 fun generatePlayers(): Players =
     try {
--- kotlin-ladder/src/main/kotlin/view/InputView.kt
+++ kotlin-ladder/src/main/kotlin/view/InputView.kt
@@ -6,4 +6,11 @@ object InputView {
         println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)")
         return readln().split(",")
     }
+
+    fun inputHeight(): Int {
+        println("최대 사다리 높이는 몇 개인가요?")
+        val input = readln()
+        require(input.toIntOrNull() != null) { "사다리의 높이는 숫자여야 합니다." }
+        return input.toInt()
+    }
 }
