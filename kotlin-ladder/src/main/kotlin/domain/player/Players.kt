a0a432414e1a7a0cfc5b0bdde44945f2fe1fbf0f
minjeong <minjeong@woowahan.com>
2023-03-09 14:20:56 +0900

    refactor: player 위치 변경

--- kotlin-ladder/src/main/kotlin/domain/player/Players.kt
+++ kotlin-ladder/src/main/kotlin/domain/player/Players.kt
@@ -6,6 +6,11 @@ class Players(val names: List<String>) {
 
     init {
         require(names.distinct().size == names.size) { "사다리 게임 참여자의 이름은 중복이 없어야합니다." }
-        value = names.map { Player(it, 0) }
+
+        val tmpValue: MutableList<Player> = ArrayList()
+        for (i in names.indices) {
+            tmpValue.add(Player(names[i], i))
+        }
+        value = tmpValue
     }
 }
